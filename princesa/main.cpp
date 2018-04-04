ae5ab70a772292efe8e5b5f9ee3e4ed4e78919be
georgecronemberger <37372000+georgecronemberger@users.noreply.github.com>
2018-04-04 11:06:01 -0300

    Add files via upload

--- princesa/main.cpp
+++ princesa/main.cpp
@@ -1,68 +1,31 @@
 #include <iostream>
-#include <random>
-
+#include <list>
 using namespace std;
-//Normal - Espada passando pro seguinte
-/*int mata_entrega(int espada, int vet[], int tamanho){
-    espada = (espada + 1) % tamanho;
 
-    while(vet[espada] == 0)
-        espada = (espada + 1) % tamanho;
+void rodar(list<int> &lista){
+    lista.push_back(lista.front());
+    lista.pop_front();
 
-    vet[espada] = 0;
-
-    while(vet[espada] == 0)
-        espada = (espada + 1) % tamanho;
-
-    return espada;
 }
-*/
-int mata_passa(int espada, int vet[], int tamanho,int saltos){
-    espada = (espada + saltos) % tamanho + 1;
-
-    while(espada == 0)
-        espada = (espada + saltos) % tamanho + 1;
-
-    vet[espada] = 0;
-
-    while(espada == 0)
-        espada = (espada + saltos) % tamanho + 1;
-
-    return espada;
-}
-
 int main()
 {
-    int qtd, inicial, vivos;
-    int i, espada;
+   int qtd = 0, escolhido = 0 ;
+   cout << "Digite qtd e escolhido" << endl;
+   cin >> qtd >> escolhido;
 
-    cout << "Quantidade de cavaleiros : ";
-    cin >> qtd;
-
-    int *vet = new int[qtd];
-
-    for(i = 1 ; i <= qtd ; i++){
-        vet[i] = i;
-    }
-
-    cout << "Informe um numero para começar: ";
-    cin >> inicial ;
-
-    vivos = qtd;
-    espada = inicial;
-
-    int saltos;
-
-    cout << "Digite o numero de saltos" << endl;
-    cin >> saltos;
-
-    while(vivos > 1){
-
-        espada = mata_passa(espada, vet, qtd,saltos);
-        vivos--;
+   list<int> lista;
+   for(int i = 1; i <= qtd; i++)
+       lista.push_back(i);
+   while(lista.front() != escolhido)
+       rodar(lista);
+    while(lista.size() > 1){
+        int j = lista.front();
+        rodar(lista);
+        lista.pop_front();
+        for( int j = 0 ; j < saltos; j++)
+            rodar(lista);
 
     }
 
-    cout << "Vencedor : " << espada << endl;
-    return 0;
+
 }
