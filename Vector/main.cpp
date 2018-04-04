e3fed5f27a1ea1ed9c4b52c29b4ec6b6f1d50c5a
georgecronemberger <37372000+georgecronemberger@users.noreply.github.com>
2018-04-04 10:31:16 -0300

    Add files via upload

---
+++ Vector/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+struct Vetor{
+    int * _data;//bloco de dados
+    int _capacidade;//tamanho maximo do vetor
+    int _size; //quantos elementos estao inseridos
+
+    Vetor(int capacidade){
+        this->_capacidade = capacidade;
+        this->_size = 0;
+        this->_data = new int[capacidade];//retorno zerado
+    }
+    void push_back(int value){
+        if(this->_size == this->_capacidade){
+            reserve(2*this->_capacidade);
+            return;
+}
+        this->_data[this->_size] = value;
+        this->_size += 1;
+
+    }
+    void pop_back(){
+        if(this->_size == 0)
+            return;
+        this->_data[_size] = 0;
+        this->_size -= 1;
+    }
+
+    //retornar a refencia à variavel dessa posicao
+    int& at(int indice){
+        return this->_data[indice];
+    }
+    int& front(){
+        return this->_data[0];
+    }
+    int& back(){
+        return this->_data[this->_size - 1];
+    }
+    int * begin(){
+        return &this->_data[0];
+    }
+    int * end(){
+        return &this->_data[this->_size];
+    }
+    int size(){
+        return this->_size;
+    }
+    int capacity(){
+        return this->_capacidade;
+    }
+
+    void reserve(int capacit){
+        int salvar[this->_capacidade]; // capacida pode ta cheia
+
+        for(int i = 0; i < this->_size; i++)
+        salvar[i] = this->_data[i];
+
+
+        if(capacit >= this->_size)
+            this->_capacidade = capacit;
+
+        else if(capacit < this->_size){
+            this->_capacidade = capacit;
+            this->_size = capacit;
+        }
+
+        for(int j = 0; j < this->_size;j++)
+            this->_data[j] = salvar[j];
+    }
+
+
+};
+
+
+#include <vector>
+int main()
+{
+
+    Vetor pt(2);
+    pt.push_back(10);
+    pt.push_back(12);
+    pt.push_back(32);
+    pt.push_back(3);
+    pt.reserve(2);
+
+    cout << pt.at(0)<<"\n";
+    cout<<pt.at(2)<<"\n";
+    cout<<pt.size()<<"\n";
+    cout<<pt.capacity()<<"\n";
+    //cout << "Hello World!" << endl;
+    return 0;
+}
